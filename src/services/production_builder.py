237ba193bf3ba32f7f0bee69ba72967895a72322
modecstap <toni.shebanin@mail.ru>
2025-04-30 14:08:16 +0300

    Refactoring: production_service логика построения вынесена в отдельный класс

---
+++ src/services/production_builder.py
@@ -0,0 +1,23 @@
+from src.server.handlers.models.production_models import ProductionModel
+from src.services import CostCalculatorService
+from src.services.mappers import ProductionMapper
+from src.storage.tables import Transaction
+
+
+class ProductionBuilder:
+    def __init__(self, mapper: ProductionMapper):
+        self._cost_calculator_service = CostCalculatorService()
+        self.mapper = mapper
+
+    async def build(self, production) -> tuple:
+        cost = await self._cost_calculator_service.calculate_production_cost(production)
+        product = self.mapper.model_to_entity(production)
+        transaction = Transaction(
+            release_date=production.release_date,
+            material_id=production.type_id,
+            product_id=product.id,
+            count=production.count,
+            price=cost,
+            remains=production.count
+        )
+        return product, transaction
--- src/services/production_service.py
+++ src/services/production_service.py
@@ -2,6 +2,7 @@ from src.server.handlers.models.production_models import ProductionModel
 from src.services.cost_calculator_service import CostCalculatorService
 from src.services.base_service import BaseService
 from src.services.mappers import ProductionMapper
+from src.services.production_builder import ProductionBuilder
 from src.services.utils import ServiceFactory, ServiceConfig
 from src.storage.repositories import TransactionRepository, ProductRepository
 from src.storage.tables import Transaction
@@ -12,26 +13,27 @@ from src.storage.tables import Transaction
     )
 )
 class ProductionService(BaseService):
-
-    def __init__(self):
-        self._main_mapper: ProductionMapper = ProductionMapper()
-        self._transaction_repository = TransactionRepository()
-        self._product_repository = ProductRepository()
-        self._cost_calculator_service = CostCalculatorService()
+    def __init__(
+        self,
+        mapper: ProductionMapper,
+        product_repo: ProductRepository,
+        transaction_repo: TransactionRepository,
+        cost_calculator: CostCalculatorService,
+        builder: ProductionBuilder,
+    ):
+        self._mapper = mapper
+        self._product_repo = product_repo
+        self._transaction_repo = transaction_repo
+        self._cost_calculator = cost_calculator
+        self._builder = builder
 
     async def write_production(self, production: ProductionModel):
-        async with self._product_repository.create_session() as session:
-            production_cost = await self._cost_calculator_service.calculate_production_cost(production)
-            product_entity = self._main_mapper.model_to_entity(production)
-            await self._product_repository.insert([product_entity], session=session)
-            await self._product_repository.insert_used_transactions(product_entity, production.count, session=session)
-            product_transaction = Transaction(
-                release_date=production.release_date,
-                material_id=production.type_id,
-                product_id=product_entity.id,
-                count=production.count,
-                price=production_cost.production_cost,
-                remains=production.count
-            )
-            await self._transaction_repository.insert([product_transaction], session=session)
+        async with self._product_repo.create_session() as session:
+
+            product, transaction = await self._builder.build(production)
+
+            await self._product_repo.insert([product], session=session)
+            await self._product_repo.insert_used_transactions(product, production.count, session=session)
+            await self._transaction_repo.insert([transaction], session=session)
+
             await session.commit()
